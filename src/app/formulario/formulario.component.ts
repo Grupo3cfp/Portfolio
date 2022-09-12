e6b0ba06a2508b8a094da9c827c8bddcdb3bdb1c
unknown <grupo3.404@gmail.com>
2022-09-12 20:28:53 -0300

    validaciones de campos

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -14,14 +14,39 @@ loginForm: FormGroup | any ;
 
   ngOnInit() {
     this.loginForm=this.formBuilder.group({
-      email : ["", [Validators.email, Validators.required]],
-      password :[
+      email : ["alguien@example.com", [Validators.email, Validators.required]],
+      mensaje :[
+        "escribe aqui",
+        [
+          Validators.required
+        ]
+      ],
+      edad : ["01234", [Validators.required]],
+      nombre :[
         "",
         [
           Validators.required
         ]
-      ]
+      ],
+
     });
   }
 
+  onLogin() {
+
+    if (this.loginForm.valid == true) {
+      alert( 'ingreso todo bien');
+      alert(this.loginForm.value.mensaje);
+      alert(this.loginForm.value.email);
+
+      window.open('https://wa.me/' + 2213543668 + '?text=' +this.loginForm.value.email + this.loginForm.value.mensaje, '_blank');
+
+    } else {
+
+      alert( 'no ingreso todo bien');
+
+    }
+  }
+
 }
+
